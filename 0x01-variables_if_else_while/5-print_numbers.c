268b2997df7c57728db34be4930a0b1054beb288
Bravecupidon <alanyoumbi99@gmail.com>
2022-10-18 07:07:39 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.H>
+
+/**
+ * main- print single numbers from base 10 starting from zero
+ * return : always 0
+ */
+
+int main(void)
+{
+
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	printf("\n");
+
+	return (0);
+}
